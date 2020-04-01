25ccbfac8f1fd25263fcb2e06c49715c1cce5105
KOBE <473278374@qq.com>
2020-04-01 16:09:03 +0800

    feat: 开发 isExist 接口

--- src/db/types.js
+++ src/db/types.js
@@ -3,11 +3,11 @@
  * @author Young
  */
 
- const Sequelize = require('sequelize')
- module.exports = {
+const Sequelize = require('sequelize')
+module.exports = {
     STRING: Sequelize.STRING,
     DECIMAL: Sequelize.DECIMAL,
     TEXT: Sequelize.TEXT,
     INTEGER: Sequelize.INTEGER,
     BOOLEAN: Sequelize.BOOLEAN
- }
+}
