3034b610079fd7e40a2a622765939c8f7d327eae
Young <634046906@qq.com>
2020-09-28 17:19:41 +0800

    fix:判断已登录的页面状态

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -4,11 +4,28 @@
  */
 const router = require('koa-router')()
 
+/**
+ * 获取用户信息
+ * @param {Object} ctx 
+ */
+function getLoginUserInfo (ctx) {
+    let data = {
+        isLogin: false
+    }
+    const userInfo = ctx.session.userInfo
+    if (!userInfo) return data
+    data = {
+        isLogin: true,
+        userName: userInfo.userName
+    }
+    return data
+}
+
 router.get('/login', async (ctx, next) => {
-    await ctx.render('login', {})
+    await ctx.render('login', getLoginUserInfo(ctx))
 })
 router.get('/register', async (ctx, next) => {
-    await ctx.render('register', {})
+    await ctx.render('register', getLoginUserInfo(ctx))
 })
 
 module.exports = router
