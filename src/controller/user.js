4e6fa3b3e85822cdd7c31442622c0a29bd1a4373
Young <634046906@qq.com>
2020-09-28 16:47:46 +0800

    feat:登录接口

--- src/controller/user.js
+++ src/controller/user.js
@@ -8,7 +8,8 @@ const { SuccessModel, ErrorModel } = require('../model/ResModel')
 const {
     registerUserNameNotExistInfo,
     registerUserNameExistInfo,
-    registerFailInfo
+    registerFailInfo,
+    loginFailInfo
 } = require('../model/ErrorInfo')
 
 const { doCrypto } = require('../utils/cryp')
@@ -54,8 +55,30 @@ async function register ({ userName, password, gender }) {
     }
 }
 
+/**
+ * 
+ * @param {Object} ctx koa2 ctx
+ * @param {String} userName 用户名
+ * @param {String} password 密码
+ */
+async function login (ctx, userName, password) {
+    const userInfo = await getUserInfo(userName, doCrypto(password))
+    console.log(userInfo)
+    if (!userInfo) {
+        // 失败
+        return new ErrorModel(loginFailInfo)
+    }
+
+    // 登录成功 
+    if (!ctx.session.userInfo) {
+        ctx.session.userInfo = userInfo
+    }
+    return new SuccessModel()
+
+}
 
 module.exports = {
     isExist,
-    register
+    register,
+    login
 }
--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -3,7 +3,7 @@
  * @author Young
  */
 const router = require('koa-router')()
-const { isExist, register } = require('../../controller/user')
+const { isExist, register, login } = require('../../controller/user')
 router.prefix('/api/user')
 const userValidate = require('../../validator/user')
 const { genValidator } = require('../../middlewares/validate')
@@ -23,4 +23,10 @@ router.post('/isExist', async (ctx, next) => {
     const { userName } = ctx.request.body
     ctx.body = await isExist(userName)
 })
+
+router.post('/login', async (ctx, next) => {
+    const { userName, password } = ctx.request.body
+    ctx.body = await login(ctx, userName, password)
+})
+
 module.exports = router
